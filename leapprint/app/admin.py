48d2a4a60a61143ef0a0ddaf3dd03064a3b317a3
Kirill Arkhipenko <kirill.a@aggrostudios.com>
2016-01-27 16:47:48 +0300

    created static root

--- leapprint/app/admin.py
+++ leapprint/app/admin.py
@@ -2,9 +2,19 @@
 
 from django.contrib import admin
 
-from models import Order
+from models import Order, Setting, File
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Setting)
+class SettingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
     pass
