20f47397529ac636b6f8f3e78ee034ec471f4265
Kirill Arkhipenko <kirill.a@aggrostudios.com>
2016-01-28 17:34:25 +0300

    added django-cors-headers

--- leapprint/core/settings.py
+++ leapprint/core/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'app'
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +134,5 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     # 'PAGE_SIZE': 10
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
